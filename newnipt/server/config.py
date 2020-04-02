f638f73117909d734b5752f0c45d4d72123c9f60
Maya <maya.brandi@scilifelab.se>
2020-04-02 20:28:37 +0200

    getting it working

--- newnipt/server/config.py
+++ newnipt/server/config.py
@@ -10,3 +10,6 @@ SECRET_KEY = os.environ['NIPT_SECRET_KEY']
 
 # analysis
 ANALYSIS_PATH = os.environ['NIPT_ANALYSIS_PATH']
+
+GOOGLE_CLIENT_ID = '527111695356-mnoqdc2e68nf19tq7o8vdibnb8lj3ge4.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'A7rEjzAjO57VYmB1EIaoO71N'
--- newnipt/server/views.py
+++ newnipt/server/views.py
@@ -1,11 +1,47 @@
 #!/usr/bin/env python
 
-from flask import url_for, redirect, render_template, request, Blueprint, current_app
+from flask import url_for, redirect, render_template, request, Blueprint, current_app, session
+from authlib.integrations.flask_client import OAuth
 
 app = current_app
 blueprint = Blueprint('server', __name__ )
 
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth = OAuth(app)
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile'
+    } 
+    )     
+       
+
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
+    user = session.get('user')
     return render_template(
-        'index.html')
+        'index.html',
+        user=user)
+
+@blueprint.route('/login')
+def login():
+    redirect_uri = url_for('server.authorized', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+    
+    
+@blueprint.route('/authorized')
+def authorized(): 
+    token = oauth.google.authorize_access_token()
+    print(token)
+    user = oauth.google.parse_id_token(token)
+    print(user)
+    session['user'] = user
+    return redirect('/')
+
+
+@blueprint.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')

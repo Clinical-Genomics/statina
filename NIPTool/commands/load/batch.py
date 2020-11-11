fb7353dbaffff7c316b7e9629405e96289d845ae
mayabrandi <maya.brandi@scilifelab.se>
2020-11-11 13:42:32 +0100

    loading string

--- NIPTool/commands/load/batch.py
+++ NIPTool/commands/load/batch.py
@@ -1,22 +1,34 @@
 import logging
 import click
-from NIPTool.load.batch import load_one_batch
+from NIPTool.load.batch import load_result_file, load_concentrastions, load_project_name
 from flask.cli import with_appcontext, current_app
 from datetime import date, timedelta
 from NIPTool.exeptions import NIPToolError
+import json
+
 
 
 LOG = logging.getLogger(__name__)
 
 
 @click.command("batch", short_help="load batch into db.")
-@click.option("-b", "--batch-path", help="path to batch load config")
+@click.option("-b", "--load-config", help="path to batch load config")
 @with_appcontext
-def batch(batch_path):
-    """Read and load lims data for one sample, all samples or the most recently updated samples."""
+def batch(load_config: dict) -> None:
+    """Read and load data for one batch.
+    
+    Args: load_config - dict with keys: 
+        "concentrations"
+        "result_file"
+        "project_name"
+    """
 
+    config_data = json.loads(load_config)
+    
     try:
-        load_one_batch(current_app.adapter, batch_path)
+        load_result_file(current_app.adapter, config_data["result_file"])
+        load_concentrastions(current_app.adapter, config_data["concentrations"])
+        load_project_name(current_app.adapter, config_data["project_name"], config_data["flowcell"])
     except NIPToolError as e:
         LOG.error(e.message)
         raise click.Abort()
--- NIPTool/load/batch.py
+++ NIPTool/load/batch.py
@@ -2,7 +2,6 @@ import logging
 from NIPTool.build.document import build_sample, build_batch
 from NIPTool.parse.batch import parse_batch_file
 from NIPTool.models.validation import requiered_fields
-import json
 
 
 LOG = logging.getLogger(__name__)
@@ -17,28 +16,38 @@ def check_requiered_fields(document):
     return True
 
 
-def load_one_batch(adapter, load_config_path: str):
-    """Function to load one lims sample into the database. 
+def load_result_file(adapter, batch_data: list) -> None:
+    """Function to load data from fluffy result file. 
     Raises:
         MissingResultsError: when parsing file that is empty"""
-    # kanske separera momentetn i tre funktioner för enklare testning
-    # läs dict inte fil
-    with open(load_config_path) as file:
-        config_data = json.load(file)
 
-    batch_data = parse_batch_file(config_data["result_file"])
-    concentrations = config_data["concentrations"]
-    project_name = config_data["project_name"]
+    batch_data = parse_batch_file(batch_data)
 
     for sample in batch_data:
         if not check_requiered_fields(sample):
             continue
         mongo_sample = build_sample(sample)
-        mongo_sample["concentration"] = concentrations[mongo_sample["_id"]]
         adapter.add_or_update_document(mongo_sample, adapter.sample_collection)
 
     if not check_requiered_fields(batch_data[0]):
         return
     mongo_batch = build_batch(batch_data[0])
+    adapter.add_or_update_document(mongo_batch, adapter.batch_collection)
+
+
+def load_concentrastions(adapter, concentrations: dict) -> None:
+    """Function to load concentrations to samples in the database."""
+
+    for sample, concentration in concentrations.items():
+        mongo_sample = adapter.sample(sample)
+        mongo_sample["concentration"] = concentration
+        adapter.add_or_update_document(mongo_sample, adapter.sample_collection)
+
+
+def load_project_name(adapter, project_name: str, flowcell: str) -> None:
+    """Function to load batch name"""
+
+    mongo_batch = adapter.batch(flowcell)
     mongo_batch["SampleProject"] = project_name
     adapter.add_or_update_document(mongo_batch, adapter.batch_collection)
+

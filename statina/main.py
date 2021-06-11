23173c823ded09dac1398daa5b2f5c0007efb023
mayabrandi <maya.brandi@scilifelab.se>
2021-06-11 12:17:24 +0200

    Little improvements

--- statina/main.py
+++ statina/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse, Response
+from fastapi import Request
 
 from statina.API.external.api.api_v1.endpoints import (
     batches,
@@ -11,12 +13,19 @@ from statina.API.external.api.api_v1.endpoints import (
     update,
 )
 from statina.API.internal.api.api_v1.endpoints import insert
-
+from statina.exeptions import CredentialsError
 
 external_app = FastAPI(
     servers=[{"url": "https://nipttol-stage.scilifelab.se", "description": "Staging environment"}],
     root_path_in_servers=False,
 )
+
+
+@external_app.exception_handler(CredentialsError)
+async def exception_handler(request: Request, exc: CredentialsError) -> Response:
+    return RedirectResponse(url="/")
+
+
 external_app.include_router(login.router, prefix="/login", tags=["login"])
 external_app.include_router(batches.router, prefix="/batches", tags=["batches"])
 external_app.include_router(index.router, tags=["index"])

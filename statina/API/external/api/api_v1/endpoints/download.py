6e8533a09aab96f1d336499355d2b84c4374eaa6
mayabrandi <maya.brandi@scilifelab.se>
2021-07-06 12:06:52 +0200

    wip

--- statina/API/external/api/api_v1/endpoints/download.py
+++ statina/API/external/api/api_v1/endpoints/download.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import FileResponse, RedirectResponse
+from starlette.responses import StreamingResponse
 
 from statina.adapter.plugin import StatinaAdapter
 from statina.API.external.api.deps import get_current_user
@@ -10,20 +11,24 @@ from statina.crud.find import find
 from statina.models.database import User, DataBaseSample
 from statina.parse.batch import validate_file_path
 from zipfile import ZIP_DEFLATED, ZipFile
+import io
+import zipfile
 from os import PathLike
 from typing import Union, Optional
 
 router = APIRouter()
 
 
-# def zip_dir(zip_name: str, source_dir: Union[str, PathLike], suffix: Optional[str] = None):
-# """Function for zipping"""
-# src_path = Path(source_dir).expanduser().resolve(strict=True)
-# with ZipFile(zip_name, "w", ZIP_DEFLATED) as zf:
-#    for file in src_path.rglob("*"):
-#        if suffix and file.suffix != suffix:
-#            continue
-#        zf.write(file, file.relative_to(src_path.parent))
+def zip_dir(zip_name: str, source_dir: Union[str, PathLike], suffix: Optional[str] = None):
+    """Function for zipping"""
+    src_path = Path(source_dir).expanduser().resolve(strict=True)
+    file_obj = io.BytesIO()
+    with ZipFile(file=file_obj, mode="w", compression=ZIP_DEFLATED, compresslevel=9) as zf:
+        for file in src_path.rglob("*"):
+            zf.writestr(zinfo_or_arcname=file, data=open(file, "r"))
+            print(file)
+        zf.write(file, file.relative_to(src_path.parent))
+    return file_obj
 
 
 @router.get("/batch_download/{batch_id}/{file_id}")
@@ -37,21 +42,20 @@ def batch_download(
     """View for batch downloads"""
     batch: dict = find.batch(adapter=adapter, batch_id=batch_id).dict()
     file_path = batch.get(file_id)
+    file_path = "/Users/maya.brandi/opt/lovedkitten/2021-02-09"
+    print(file_path)
     if not validate_file_path(file_path):
         return RedirectResponse(request.headers.get("referer"))
 
     path = Path(file_path)
     if path.is_dir():
-        return RedirectResponse(request.headers.get("referer"))
-        # needs to be fixed
-        # zip_file_name = f"{batch_id}_segmental_calls.zip"
-        # path = Path(zip_file_name)
-        # zip_dir(zip_name=zip_file_name, source_dir=file_path, suffix="bed")
-        # return FileResponse(
-        #    str(path.absolute()),
-        #    media_type="application/x-zip-compressed",
-        #    headers={"Content-Disposition": f"attachment;filename={zip_file_name}"},
-        # )
+        print("hej")
+        zip_file_name = f"{batch_id}_segmental_calls.zip"
+        print(zip_file_name)
+
+        file_obj = zip_dir(zip_name=zip_file_name, source_dir=file_path, suffix="bed")
+        print(file_obj)
+        return StreamingResponse(file_obj, media_type="application/text")
 
     return FileResponse(
         str(path.absolute()), media_type="application/octet-stream", filename=path.name

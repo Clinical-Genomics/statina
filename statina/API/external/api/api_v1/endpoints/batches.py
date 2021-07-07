090af304546221eda60137bed785743ea7c2030c
mayabrandi <maya.brandi@scilifelab.se>
2021-07-07 11:09:19 +0200

    treshold for ffx

--- statina/API/external/api/api_v1/endpoints/batches.py
+++ statina/API/external/api/api_v1/endpoints/batches.py
@@ -177,8 +177,8 @@ def fetal_fraction_XY(
         context=dict(
             sex_thresholds={
                 "XY_fetal_fraction_y": sex_thresholds.XY_fetal_fraction_y(),
-                # "XX_lower": sex_thresholds.XX_lower(),
-                # "XX_upper": sex_thresholds.XX_upper(),
+                "XX_lower": sex_thresholds.XX_lower(),
+                "XX_upper": sex_thresholds.XX_upper(),
                 # "XY_lower": sex_thresholds.XY_lower(),
                 # "XY_upper": sex_thresholds.XY_upper(),
                 # "XXY": sex_thresholds.XXY(),
--- statina/API/external/constants.py
+++ statina/API/external/constants.py
@@ -35,8 +35,8 @@ FF_TRESHOLDS = {
 SEX_THRESHOLDS = {
     "y_min": -1,
     "y_max": 20,
-    "xx_lower": 2,
-    "xx_upper": 4,
+    "xx_lower": -1,
+    "xx_upper": 3.4,
     "xy_lowest": 0.6,
     "k_upper": 1.51,
     "k_lower": 1.49,

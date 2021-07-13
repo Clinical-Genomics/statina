21d781bd5a2d865e8cba892b377d677fde503ac2
Github CI <41898282+github-actions[bot]@users.noreply.github.com>
2021-07-13 12:35:27 +0000

    Bump version: 1.2.4 → 1.2.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="statina",
-    version="1.2.4",
+    version="1.2.5",
     description="NIPT data storage and visualisation",
     long_description=long_description,
     long_description_content_type="text/markdown",

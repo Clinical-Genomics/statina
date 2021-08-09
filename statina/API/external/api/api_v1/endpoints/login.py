81458d3e82be217858b1697f126829b286420c28
Mropat <mariaropat@gmail.com>
2021-08-09 16:47:13 +0200

    update before sending mail

--- statina/API/external/api/api_v1/endpoints/login.py
+++ statina/API/external/api/api_v1/endpoints/login.py
@@ -50,6 +50,8 @@ async def validate_user(
         )
     elif update_user.verification_hex == verification_hex and update_user.role == "unconfirmed":
         try:
+            update_user.role = "inactive"
+            update.update_user(adapter=adapter, user=update_user)
             email_form = FormDataRequest(
                 sender_prefix=email_settings.sender_prefix,
                 request_uri=email_settings.mail_uri,
@@ -59,8 +61,6 @@ async def validate_user(
                 f'Follow <a href="{email_settings.website_uri}">link</a> to activate user',
             )
             email_form.submit()
-            update_user.role = "inactive"
-            update.update_user(adapter=adapter, user=update_user)
             response.set_cookie(
                 key="user_info",
                 value="Email confirmed! Your account will be activated after manual review",

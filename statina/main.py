208f98a0234846be240033f1d998dc162719b52f
mayabrandi <maya.brandi@scilifelab.se>
2021-08-23 09:55:10 +0200

    merging external v2 with internal

--- statina/main.py
+++ statina/main.py
@@ -5,10 +5,9 @@ from fastapi import Request
 from statina.exeptions import CredentialsError
 
 import statina.API.external.api.api_v1.endpoints as external_api_v1
-import statina.API.external.api.api_v2.endpoints as external_api_v2
 import statina.API.internal.api.api_v1.endpoints as internal_api_v1
 
-external_versions = {"v1": external_api_v1, "v2": external_api_v2}
+external_versions = {"v1": external_api_v1}
 internal_versions = {"v1": internal_api_v1}
 
 
@@ -26,7 +25,11 @@ def external(version: str) -> FastAPI:
         return RedirectResponse(url="/")
 
     external_app.include_router(api.login.router, prefix="/login", tags=["login"])
-    external_app.include_router(api.batches.router, prefix="/batches", tags=["batches"])
+    external_app.include_router(
+        api.batches.router,
+        prefix="/batches",
+        tags=["batches"],
+    )
     external_app.include_router(api.index.router, tags=["index"])
     external_app.include_router(api.sample.router, tags=["sample"])
     external_app.include_router(api.update.router, tags=["update"])
@@ -39,9 +42,19 @@ def internal(version: str) -> FastAPI:
     api = internal_versions[version]
     internal_app = FastAPI()
     internal_app.include_router(api.insert.router, prefix="/insert", tags=["insert"])
+    internal_app.include_router(api.login.router, prefix="/login", tags=["login"])
+    internal_app.include_router(
+        api.batches.router,
+        prefix="/batches",
+        tags=["batches"],
+    )
+    internal_app.include_router(api.index.router, tags=["index"])
+    internal_app.include_router(api.sample.router, tags=["sample"])
+    internal_app.include_router(api.update.router, tags=["update"])
+    internal_app.include_router(api.download.router, tags=["download"])
+    internal_app.include_router(api.statistics.router, tags=["statistics"])
     return internal_app
 
 
 external_v1_app = external("v1")
-external_v2_app = external("v2")
 internal_v1_app = internal("v1")

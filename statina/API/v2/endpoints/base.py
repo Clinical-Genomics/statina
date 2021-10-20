125ebff72c17a36038bcf4911ff41fa869235cee
Mropat <mariaropat@gmail.com>
2021-10-20 11:17:47 +0200

    return dict not json

--- statina/API/v2/endpoints/base.py
+++ statina/API/v2/endpoints/base.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from starlette.responses import JSONResponse
-
 from statina.API.external.api.deps import authenticate_user, create_access_token
+
 from statina.API.v2.endpoints.user import credentials_exception
 from statina.models.database import User
 from statina.models.server.auth import Token
@@ -24,4 +24,4 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     if not user:
         raise credentials_exception
     access_token = create_access_token(form_data=form_data, username=user.username)
-    return JSONResponse(content=Token(access_token=access_token, token_type="bearer").json())
+    return Token(access_token=access_token, token_type="bearer").dict()

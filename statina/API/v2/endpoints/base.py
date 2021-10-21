361c53c10da5d188b65d3bcd5efae0ac01af37cc
Mropat <mariaropat@gmail.com>
2021-10-21 14:45:27 +0200

    remove imports

--- statina/API/v2/endpoints/base.py
+++ statina/API/v2/endpoints/base.py
@@ -1,12 +1,6 @@
-from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter
 from starlette.responses import JSONResponse
-from statina.API.external.api.deps import authenticate_user, create_access_token
-
-from statina.API.v2.endpoints.user import credentials_exception
 from statina.constants import SCOPES
-from statina.models.database import User
-from statina.models.server.auth import Token
 
 router = APIRouter(prefix="/v2")
 

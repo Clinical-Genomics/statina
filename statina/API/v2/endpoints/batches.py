361c9163cc180feae42d52bcb710796e7cbb4626
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-04-09 10:33:55 +0200

    New Ratio plot endpoint (#251)
    Renamed endpoint
    
    ---------
    
    Co-authored-by: islean <isak.ohlsson.angnell@gmail.com>

--- statina/API/v2/endpoints/batches.py
+++ statina/API/v2/endpoints/batches.py
@@ -20,7 +20,7 @@ from statina.crud.find.plots.coverage_plot_data import (
     get_box_data_for_coverage_plot,
     get_scatter_data_for_coverage_plot,
 )
-from statina.crud.find.plots.zscore_plot_data import (
+from statina.crud.find.plots.ratio_plot_data import (
     get_tris_control_abnormal,
     get_tris_control_normal,
     get_tris_samples,
@@ -151,14 +151,14 @@ def batch_download(
     return response
 
 
-@router.get("/batch/{batch_id}/zscore_plot")
-def zscore_plot(
+@router.get("/batch/{batch_id}/ratio_plot")
+def ratio_plot(
     batch_id: str,
     ncv: Literal["13", "18", "21"] = Query(...),
     current_user: User = Security(get_current_active_user, scopes=["R"]),
     adapter: StatinaAdapter = Depends(get_nipt_adapter),
 ):
-    """Batch view with with Zscore plot"""
+    """Batch view with with Ratio plot"""
 
     dataset = get_dataset(adapter=adapter, batch_id=batch_id)
     return JSONResponse(
